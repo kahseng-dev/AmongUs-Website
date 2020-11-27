3c492788bdde94c372f9ab6e0d12c23d0956dd00
kahseng <S10208180@connect.np.edu.sg>
2020-11-27 12:56:47 +0800

    Added comments for all code

--- script/script-maps.js
+++ script/script-maps.js
@@ -1,4 +1,4 @@
-function show_info(x) {
+function show_info(x) { // For navigation bar to switch to appropriate description when cycling through maps.
     var descriptions = ["skeld-description", "miraHQ-description", "polus-description"];
     for (var i = 0; i < descriptions.length; i++) {
         document.getElementById(descriptions[i]).style.display='none';
@@ -6,8 +6,11 @@ function show_info(x) {
     document.getElementById(descriptions[x]).style.display='block';
 }
 
-var descriptions = ["skeld-description", "miraHQ-description", "polus-description"];
-for (var i = 0; i < descriptions.length; i++) {
+var descriptions = ["skeld-description", "miraHQ-description", "polus-description"]; // array of descriptions ids
+
+for (var i = 0; i < descriptions.length; i++) { // set every description to display none first.
     document.getElementById(descriptions[i]).style.display='none';
 }
+
+// when map page loads it will default to the skeld description
 document.onload(document.getElementById("skeld-description").style.display='block');
--- script/script.js
+++ script/script.js
@@ -1,16 +1,16 @@
-document.querySelector('.close').addEventListener('click',
+document.querySelector('.close').addEventListener('click', // When user clicks on close button, it will set modal window to display none
 function() {
     document.querySelector('.bg-modal').style.display='none';
 });
 
-var modal = document.querySelector(".bg-modal");
-var btn = document.getElementById("get-it");
+var modal = document.querySelector(".bg-modal"); // variable for modal window class
+var btn = document.getElementById("get-it"); // variable for button class
 
-btn.onclick = function() {
+btn.onclick = function() { // when button is clicked modal window will display flex
     modal.style.display = "flex";
 }
 
-window.onclick = function(event) {
+window.onclick = function(event) { // when user clicks outside of the modal window it will also close the window.
     if (event.target == modal) {
         modal.style.display = "none";
     }
